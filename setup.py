44f2c7b0d7b018bd56c936987381b6cdeb77c260
Renat Galimov <renat2017@gmail.com>
2014-07-14 16:01:52 +0400

    bumped version to 0.1.0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='cms-special-offers',
-    version='0.1',
+    version='0.1.0.1',
     packages=['cms_special_offers'],
     include_package_data=True,
     description=locals()['__doc__'],
