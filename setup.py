9d517a0fc6b84476bd70df2a28e669615dc96690
Renat Galimov <renat2017@gmail.com>
2014-07-17 15:46:24 +0400

    Use find_packages instead of writing setup packages by hand

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@
 'django-cms plugin providing special offers functionality.'
 
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='cms-special-offers',
-    version='0.1',
-    packages=['cms_special_offers'],
+    version='0.1.0.1',
+    packages=find_packages(),
     include_package_data=True,
     description=locals()['__doc__'],
     url='https://github.com/ITrex/cms-special-offers/',
